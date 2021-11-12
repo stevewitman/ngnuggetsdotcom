c55f793ce9c47a25d27f8f178e57f189ff04ded6
Steve Witman <stevewitman@gmail.com>
2021-11-11 17:37:17 -0700

    add angular material
      - add angular material
      - convert buttons to material in home/home-page and admin/add-post-page

--- src/app/feat/admin/admin.module.ts
+++ src/app/feat/admin/admin.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AdminRoutingModule } from './admin-routing.module';
 import { AddPostPageComponent } from './admin-add-post/add-post-page.component';
 
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [AddPostPageComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  imports: [CommonModule, AdminRoutingModule, MatButtonModule],
   exports: [AddPostPageComponent],
 })
 export class AdminModule {}
--- src/app/feat/home/home.module.ts
+++ src/app/feat/home/home.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
 
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, MatButtonModule],
   exports: [HomePageComponent],
 })
 export class HomeModule {}
